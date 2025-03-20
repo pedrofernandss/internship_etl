56b70ca3d858bf1b945b0c6c77d3d353ac7adad7
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-03-20 20:36:14 -0300

    formatting informations

--- intern/remotar.py
+++ intern/remotar.py
@@ -9,13 +9,35 @@ jobs_list = data.get("data", [])
 
 for job in jobs_list:
     creation_date = job.get("createdAt")
-    current_date = datetime.now()
+    creation_date = datetime.fromisoformat(creation_date)
+    current_date = datetime.now(creation_date.tzinfo)
 
     time_difference = current_date - creation_date
         
     if time_difference < timedelta(days=10):
         title = job.get("title")
-        description = job.get("descrption")
+        description = job.get("description")       
         link = job.get("externalLink")
+
+        company = job.get("company")
+        company_name = company.get("name")
+
+        print(f"""
+              A empresa {company_name} anunciou uma nova vaga para {title}! ✨✨
+
+                    🧑‍🎓 Nível: Estágio
+                    📍 Localidade: Remoto
+
+                    Descrição da vaga:
+
+                    {description}
+
+                    
+                    🌐 Link: {link}
+
+                    Caso decida se inscrever, não esqueça de personalizar seu currículo! 😉
+
+                    Que a sorte esteja sempre a seu favor! 🤗
+              """)
 
     
