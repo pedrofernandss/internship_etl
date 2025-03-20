0efe4456ee90b32f4b903d4acb0ae6a26e3fb5f7
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-03-20 20:57:39 -0300

    calculate date difference in a function

--- remotar_jobs/entry_level/main.py
+++ remotar_jobs/entry_level/main.py
@@ -1,6 +1,18 @@
 import requests
 from datetime import datetime, timedelta
 
+def is_new_job(creation_date: str) -> bool:
+    creation_date = datetime.fromisoformat(creation_date)
+    current_date = datetime.now(creation_date.tzinfo)
+
+    time_difference = current_date - creation_date
+        
+    if time_difference < timedelta(days=10):
+        return True
+    else: 
+        return False
+    
+
 url = "https://api.remotar.com.br/jobs?search=&tagId=17&categoryId=4,7,15,13,14"
 response = requests.get(url)
 
@@ -9,12 +21,8 @@ jobs_list = data.get("data", [])
 
 for job in jobs_list:
     creation_date = job.get("createdAt")
-    creation_date = datetime.fromisoformat(creation_date)
-    current_date = datetime.now(creation_date.tzinfo)
-
-    time_difference = current_date - creation_date
         
-    if time_difference < timedelta(days=10):
+    if is_new_job(creation_date):
         title = job.get("title")
         description = job.get("description")       
         link = job.get("externalLink")
