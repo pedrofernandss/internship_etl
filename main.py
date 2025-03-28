7a678ab515a2eecdb15391f7f42daab4ec6d3ad1
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-03-28 15:24:47 -0300

    bot sending scrapper message

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import discord 
 from discord.ext import commands
 
+from remotar_jobs.intern.main import clean_data, get_jobs
+
 discord_token = os.environ['DISCORD_TOKEN']
 channel_id = int(os.environ['CHANNEL_ID'])
 
@@ -15,8 +17,14 @@ async def send_scheduled_message():
     await bot.wait_until_ready()
     channel = bot.get_channel(channel_id)
     while not bot.is_closed():
-        await asyncio.sleep(2)
-        await channel.send("Mensagem programada!")
+        jobs = get_jobs()
+        new_jobs_messages = clean_data(jobs)
+        
+        for job_message in new_jobs_messages:
+            await channel.send(job_message)
+        
+        if not new_jobs_messages:
+            print("Nenhuma vaga nova encontrada.")
         
 @bot.event
 async def on_ready():
