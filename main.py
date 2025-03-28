faa6cc459137c6e964159a9c4e77a59a70f2fe64
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-03-28 19:20:59 -0300

    formatting message to be send

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import discord 
 from discord.ext import commands
 
-from remotar_jobs.intern.main import clean_data, get_jobs
+from remotar_jobs.intern.main import clean_data
 
 discord_token = os.environ['DISCORD_TOKEN']
 channel_id = int(os.environ['CHANNEL_ID'])
@@ -16,16 +16,26 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def send_scheduled_message():
     await bot.wait_until_ready()
     channel = bot.get_channel(channel_id)
+
     while not bot.is_closed():
-        jobs = get_jobs()
-        new_jobs_messages = clean_data(jobs)
-        
-        for job_message in new_jobs_messages:
-            await channel.send(job_message)
-        
-        if not new_jobs_messages:
-            print("Nenhuma vaga nova encontrada.")
-        
+        new_jobs = clean_data()
+
+        if new_jobs:
+            for job in new_jobs:
+                job_message = (
+                f"✨ **Nova vaga disponível!** ✨\n\n"
+                f"A empresa **{job['company_name']}** abriu uma nova vaga\n\n"
+                f"🧑‍🎓 Nível: **Estágio**\n"
+                f"📌 Cargo: **{job['title']}**\n"
+                f"📍 Localidade: **Remoto**\n"
+                f"🔗 [Acesse a vaga aqui]({job['link']})\n\n" 
+                f"Caso decida se inscrever, não esqueça de personalizar seu currículo! 😉\n"
+                )
+                await channel.send(job_message)
+                await asyncio.sleep(5)
+
+        await asyncio.sleep(5) 
+                
 @bot.event
 async def on_ready():
     bot.loop.create_task(send_scheduled_message())
