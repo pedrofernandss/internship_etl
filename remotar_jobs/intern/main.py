8de4ec198eb109eb5785183b97e46ca75c30c02e
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-03-28 18:57:08 -0300

    save data on db

--- remotar_jobs/intern/main.py
+++ remotar_jobs/intern/main.py
@@ -2,6 +2,8 @@ from typing import List
 import requests
 from datetime import datetime, timedelta
 
+from database.main import save_data
+
 def request_jobs_list():
     url = "https://api.remotar.com.br/jobs?search=&tagId=10&categoryId=4,7,15,13,14"
     response = requests.get(url)
@@ -37,6 +39,7 @@ def clean_data():
                 "created_at": creation_date
             }
 
+        save_data(job_data)
         recent_jobs.append(job_data)
 
     return recent_jobs
