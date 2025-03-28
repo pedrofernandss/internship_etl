cad59c8e2de7328897c3c364d0db299abb968a78
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-03-28 14:51:09 -0300

    make bot send scheduled message

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,16 +1,25 @@
+import asyncio
 import os
 import discord 
 from discord.ext import commands
+
+discord_token = os.environ['DISCORD_TOKEN']
+channel_id = int(os.environ['CHANNEL_ID'])
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-discord_token = os.environ['DISCORD_TOKEN']
-
-@bot.command()
-async def inverse(ctx, message):
-    await ctx.send(message[::-1])
+async def send_scheduled_message():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(channel_id)
+    while not bot.is_closed():
+        await asyncio.sleep(2)
+        await channel.send("Mensagem programada!")
+        
+@bot.event
+async def on_ready():
+    bot.loop.create_task(send_scheduled_message())
 
 bot.run(discord_token)
