6ace3c74ccee21217aa6bfd48430de13fbc8e3a5
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-06-23 23:07:28 -0300

    renaming files

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+import os
+import discord 
+from discord.ext import commands
+from dotenv import load_dotenv
+from scripts.remotar import clean_data
+
+load_dotenv()
+
+discord_token = os.environ['DISCORD_TOKEN']
+channel_ids = [int(id_) for id_ in os.environ['CHANNEL_ID'].split(',')]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+async def send_scheduled_message():
+    await bot.wait_until_ready()
+    channels = [bot.get_channel(id_) for id_ in channel_ids]
+
+    while not bot.is_closed():
+        new_jobs = clean_data()
+
+        if new_jobs:
+            for job in new_jobs:
+                job_message = (
+                f"✨ **Nova vaga disponível!** ✨\n\n"
+                f"A empresa **{job['company_name']}** abriu uma nova vaga\n\n"
+                f"🧑‍🎓 Nível: **{job['tag']}**\n"
+                f"📌 Cargo: **{job['title']}**\n"
+                f"📍 Localidade: **Remoto**\n"
+                f"🔗 [Acesse a vaga aqui]({job['link']})\n\n" 
+                f"Caso decida se inscrever, não esqueça de personalizar seu currículo! 😉\n"
+                )
+                for channel in channels:
+                    if channel:
+                        await channel.send(job_message)
+                await asyncio.sleep(60)
+
+        await asyncio.sleep(3600) 
+                
+@bot.event
+async def on_ready():
+    bot.loop.create_task(send_scheduled_message())
+
+def main():
+    bot.run(discord_token)
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/remotar.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime, timedelta
+
+from database.main import is_registered, save_data
+
+def request_jobs_list():
+    url = "https://api.remotar.com.br/jobs?search=&tagId=10,17&categoryId=4,7,8,15,13,14"
+    response = requests.get(url)
+
+    return response
+
+def is_recent_job(creation_date: str) -> bool:
+    creation_date = datetime.fromisoformat(creation_date)
+    current_date = datetime.now(creation_date.tzinfo)
+
+    time_difference = current_date - creation_date
+        
+    if time_difference < timedelta(days=2):
+        return True
+    else: 
+        return False
+    
+def clean_data():
+    data = request_jobs_list().json()
+    jobs_list = data.get("data", [])
+
+    recent_jobs = []
+
+    for job in jobs_list:
+        creation_date = job.get("createdAt")
+            
+        if is_recent_job(creation_date):
+            job_id = job.get("id")
+
+            if is_registered(job_id):
+                continue
+
+            tags = job.get("jobTags")
+
+            if tags:
+                for tag in tags:
+                    tag_id = tag['tag']['id']
+                    if tag_id == 17:
+                        tag_name = "Junior"
+                    elif tag_id == 10:
+                        tag_name = "Estágio"
+                        
+            job_data = {
+                "id": job.get("id"),
+                "title": job.get("title"),       
+                "link": job.get("externalLink"),
+                "company_name": job.get("company", {}).get("name"),
+                "tag": tag_name,
+                "created_at": creation_date
+            }
+
+            save_data(job_data)
+            recent_jobs.append(job_data)
+
+    return recent_jobs
